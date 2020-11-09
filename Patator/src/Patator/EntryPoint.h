fa96547b5d5fb78b6d09351423e6ae08b943362b
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-11-09 16:20:58 +0100

    Creation du logger et le dll se copie tout seul maintenant

--- Patator/src/Patator/EntryPoint.h
+++ Patator/src/Patator/EntryPoint.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Application.h"
+#include "Log.h"
 
 extern pat::Application* pat::createApplication();
 
 int main(int argc, char** argv) {
+	pat::Log::init();
+	PAT_CORE_WARN("Initialized log!");
+	PAT_INFO("Hello world!");
 	pat::Application* app = pat::createApplication();
 	app->run();
 	delete(app);
---
+++ Patator/src/Patator/Log.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace pat {
+
+	class PAT_API Log
+	{
+	public:
+		static void init();
+		
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core logger
+#define PAT_CORE_TRACE(...)     ::pat::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define PAT_CORE_INFO(...)      ::pat::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define PAT_CORE_WARN(...)      ::pat::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define PAT_CORE_ERROR(...)     ::pat::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define PAT_CORE_FATAL(...)     ::pat::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client logger
+#define PAT_TRACE(...)   ::pat::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define PAT_INFO(...)    ::pat::Log::GetClientLogger()->info(__VA_ARGS__)
+#define PAT_WARN(...)    ::pat::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define PAT_ERROR(...)   ::pat::Log::GetClientLogger()->error(__VA_ARGS__)
+#define PAT_FATAL(...)   ::pat::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
