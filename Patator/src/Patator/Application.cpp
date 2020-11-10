9cb1866bc60e27891fab0e854a2cda7866a638fb
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-11-11 00:22:09 +0100

    Event test deleted

--- Patator/src/Patator/Application.cpp
+++ Patator/src/Patator/Application.cpp
@@ -1,8 +1,5 @@
 #include "Application.h"
 
-//Temporary includes, to remove
-#include "Patator/Events/ApplicationEvent.h"
-#include "Patator/Log.h"
 
 namespace pat {
 
@@ -15,8 +12,6 @@ namespace pat {
 	}
 
 	void Application::run() {
-		WindowResizeEvent e(1280, 720);
-		PAT_TRACE(e);
 		while (true);
 	}
 }
