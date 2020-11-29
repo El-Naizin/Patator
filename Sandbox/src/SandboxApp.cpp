10807c80a9e42e2bb8c22a95884c14995df4f85e
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-11-29 21:04:47 +0100

    test: Ajout d'un programme pour tester une layer dans Sandbox

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,10 +1,27 @@
+#include <patpch.h>
 #include <Patator.h>
+
+class ExampleLayer : public pat::Layer {
+public:
+	ExampleLayer()
+		: Layer("Example") {
+
+	}
+
+	void onUpdate() override {
+		PAT_INFO("ExampleLayer::onUpdate");
+	}
+
+	void onEvent(pat::Event& event) override {
+		PAT_TRACE("ExampleLayer::onEvent: {0}", event);
+	}
+};
 
 class App : public pat::Application {
 public:
 
 	App() {
-
+		pushLayer(new ExampleLayer());
 	}
 
 	~App() {
