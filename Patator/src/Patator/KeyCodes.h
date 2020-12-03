df589b6d5f4e0795b55970971be8b05086a300f5
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-12-03 21:59:03 +0100

    Added Patator specific Keycodes and MouseButtonCodes, need to use them now

---
+++ Patator/src/Patator/KeyCodes.h
@@ -0,0 +1,131 @@
+#pragma once
+
+// Ce fichier a vole tous les keycodes de glfw
+// Yoink hihihi
+
+/* The unknown key */
+#define PAT_KEY_UNKNOWN            -1
+
+/* Printable keys */
+#define PAT_KEY_SPACE              32
+#define PAT_KEY_APOSTROPHE         39  /* ' */
+#define PAT_KEY_COMMA              44  /* , */
+#define PAT_KEY_MINUS              45  /* - */
+#define PAT_KEY_PERIOD             46  /* . */
+#define PAT_KEY_SLASH              47  /* / */
+#define PAT_KEY_0                  48
+#define PAT_KEY_1                  49
+#define PAT_KEY_2                  50
+#define PAT_KEY_3                  51
+#define PAT_KEY_4                  52
+#define PAT_KEY_5                  53
+#define PAT_KEY_6                  54
+#define PAT_KEY_7                  55
+#define PAT_KEY_8                  56
+#define PAT_KEY_9                  57
+#define PAT_KEY_SEMICOLON          59  /* ; */
+#define PAT_KEY_EQUAL              61  /* = */
+#define PAT_KEY_A                  65
+#define PAT_KEY_B                  66
+#define PAT_KEY_C                  67
+#define PAT_KEY_D                  68
+#define PAT_KEY_E                  69
+#define PAT_KEY_F                  70
+#define PAT_KEY_G                  71
+#define PAT_KEY_H                  72
+#define PAT_KEY_I                  73
+#define PAT_KEY_J                  74
+#define PAT_KEY_K                  75
+#define PAT_KEY_L                  76
+#define PAT_KEY_M                  77
+#define PAT_KEY_N                  78
+#define PAT_KEY_O                  79
+#define PAT_KEY_P                  80
+#define PAT_KEY_Q                  81
+#define PAT_KEY_R                  82
+#define PAT_KEY_S                  83
+#define PAT_KEY_T                  84
+#define PAT_KEY_U                  85
+#define PAT_KEY_V                  86
+#define PAT_KEY_W                  87
+#define PAT_KEY_X                  88
+#define PAT_KEY_Y                  89
+#define PAT_KEY_Z                  90
+#define PAT_KEY_LEFT_BRACKET       91  /* [ */
+#define PAT_KEY_BACKSLASH          92  /* \ */
+#define PAT_KEY_RIGHT_BRACKET      93  /* ] */
+#define PAT_KEY_GRAVE_ACCENT       96  /* ` */
+#define PAT_KEY_WORLD_1            161 /* non-US #1 */
+#define PAT_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define PAT_KEY_ESCAPE             256
+#define PAT_KEY_ENTER              257
+#define PAT_KEY_TAB                258
+#define PAT_KEY_BACKSPACE          259
+#define PAT_KEY_INSERT             260
+#define PAT_KEY_DELETE             261
+#define PAT_KEY_RIGHT              262
+#define PAT_KEY_LEFT               263
+#define PAT_KEY_DOWN               264
+#define PAT_KEY_UP                 265
+#define PAT_KEY_PAGE_UP            266
+#define PAT_KEY_PAGE_DOWN          267
+#define PAT_KEY_HOME               268
+#define PAT_KEY_END                269
+#define PAT_KEY_CAPS_LOCK          280
+#define PAT_KEY_SCROLL_LOCK        281
+#define PAT_KEY_NUM_LOCK           282
+#define PAT_KEY_PRINT_SCREEN       283
+#define PAT_KEY_PAUSE              284
+#define PAT_KEY_F1                 290
+#define PAT_KEY_F2                 291
+#define PAT_KEY_F3                 292
+#define PAT_KEY_F4                 293
+#define PAT_KEY_F5                 294
+#define PAT_KEY_F6                 295
+#define PAT_KEY_F7                 296
+#define PAT_KEY_F8                 297
+#define PAT_KEY_F9                 298
+#define PAT_KEY_F10                299
+#define PAT_KEY_F11                300
+#define PAT_KEY_F12                301
+#define PAT_KEY_F13                302
+#define PAT_KEY_F14                303
+#define PAT_KEY_F15                304
+#define PAT_KEY_F16                305
+#define PAT_KEY_F17                306
+#define PAT_KEY_F18                307
+#define PAT_KEY_F19                308
+#define PAT_KEY_F20                309
+#define PAT_KEY_F21                310
+#define PAT_KEY_F22                311
+#define PAT_KEY_F23                312
+#define PAT_KEY_F24                313
+#define PAT_KEY_F25                314
+#define PAT_KEY_KP_0               320
+#define PAT_KEY_KP_1               321
+#define PAT_KEY_KP_2               322
+#define PAT_KEY_KP_3               323
+#define PAT_KEY_KP_4               324
+#define PAT_KEY_KP_5               325
+#define PAT_KEY_KP_6               326
+#define PAT_KEY_KP_7               327
+#define PAT_KEY_KP_8               328
+#define PAT_KEY_KP_9               329
+#define PAT_KEY_KP_DECIMAL         330
+#define PAT_KEY_KP_DIVIDE          331
+#define PAT_KEY_KP_MULTIPLY        332
+#define PAT_KEY_KP_SUBTRACT        333
+#define PAT_KEY_KP_ADD             334
+#define PAT_KEY_KP_ENTER           335
+#define PAT_KEY_KP_EQUAL           336
+#define PAT_KEY_LEFT_SHIFT         340
+#define PAT_KEY_LEFT_CONTROL       341
+#define PAT_KEY_LEFT_ALT           342
+#define PAT_KEY_LEFT_SUPER         343
+#define PAT_KEY_RIGHT_SHIFT        344
+#define PAT_KEY_RIGHT_CONTROL      345
+#define PAT_KEY_RIGHT_ALT          346
+#define PAT_KEY_RIGHT_SUPER        347
+#define PAT_KEY_MENU               348
---
+++ Patator/src/Patator/MouseButtonCodes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Yoink hihihi
+// On a tout vole de glfw, ENCORE
+
+#define PAT_MOUSE_BUTTON_1         0
+#define PAT_MOUSE_BUTTON_2         1
+#define PAT_MOUSE_BUTTON_3         2
+#define PAT_MOUSE_BUTTON_4         3
+#define PAT_MOUSE_BUTTON_5         4
+#define PAT_MOUSE_BUTTON_6         5
+#define PAT_MOUSE_BUTTON_7         6
+#define PAT_MOUSE_BUTTON_8         7
+#define PAT_MOUSE_BUTTON_LAST      PAT_MOUSE_BUTTON_8
+#define PAT_MOUSE_BUTTON_LEFT      PAT_MOUSE_BUTTON_1
+#define PAT_MOUSE_BUTTON_RIGHT     PAT_MOUSE_BUTTON_2
+#define PAT_MOUSE_BUTTON_MIDDLE    PAT_MOUSE_BUTTON_3
+
